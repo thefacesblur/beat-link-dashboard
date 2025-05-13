9e6ed41e7e80d4a6791e4acffe7bf8f63f0a4a55
adam <adam.graetz@gmail.com>
2025-05-12 20:33:31 -0400

    Renamed dashboard title to be consistent with new naming and aligned it better

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
   const { data, error } = useParamsData();
 
   return (
-    <Container maxWidth="lg" sx={{ pt: 4 }}>
-      <Typography variant="h3" gutterBottom>
-        Pro DJ Link Dashboard
+    <Container width="100%" maxWidth="lg" sx={{ pt: 4 }}>
+      <Typography variant="h4" sx={{ textAlign: 'left', marginLeft: '30px' }}>
+        Beat Link Dashboard
       </Typography>
       {error && <Alert severity="error">{error.message}</Alert>}
-      {!data && !error && (
+      {!data && !error && ( 
         <Box display="flex" justifyContent="center" alignItems="center" minHeight="30vh">
           <CircularProgress />
         </Box>
